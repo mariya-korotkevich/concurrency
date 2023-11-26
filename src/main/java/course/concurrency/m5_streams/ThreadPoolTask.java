841ef27231f6cc261eeaae38dac00b359814372e
maria korotkevich <korotkevich.mv@esphere.ru>
2023-11-26 10:44:32 +0300

    задача 5 2/3

--- src/main/java/course/concurrency/m5_streams/ThreadPoolTask.java
+++ src/main/java/course/concurrency/m5_streams/ThreadPoolTask.java
@@ -6,11 +6,17 @@ public class ThreadPoolTask {
 
     // Task #1
     public ThreadPoolExecutor getLifoExecutor() {
-        return null;
+        return new ThreadPoolExecutor(0, Integer.MAX_VALUE, 60L, TimeUnit.SECONDS, new LinkedBlockingDeque<>() {
+            @Override
+            public boolean offer(Runnable runnable) {
+                return offerFirst(runnable);
+            }
+        });
     }
 
     // Task #2
     public ThreadPoolExecutor getRejectExecutor() {
-        return null;
+        return new ThreadPoolExecutor(8, 8, 0L, TimeUnit.MILLISECONDS, new SynchronousQueue<>(), (r, executor) -> {
+        });
     }
 }
